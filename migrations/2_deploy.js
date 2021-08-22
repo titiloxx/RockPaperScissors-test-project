f75f3d7980e6a85b4e23e081f0d859536985ddd6
titiloxx <titiloxx@gmail.com>
2021-08-23 01:01:51 +0200

    Cleaned some files

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,9 +1,11 @@
 const RockPaperScissors = artifacts.require("RockPaperScissors");
-//const RockPaperScissors = artifacts.require("RockPaperScissors");
 const { deployProxy } = require('@openzeppelin/truffle-upgrades');
+const { upgradeProxy } = require('@openzeppelin/truffle-upgrades');
 
 module.exports = async function (deployer) {
   //const existing = await Box.deployed();
   //Deploy main contract with proxy
   const instance = await deployProxy(RockPaperScissors,{kind:'uups'});
+  /*const exists= await RockPaperScissors.deployed()
+  const instance = await upgradeProxy(exists.address,RockPaperScissors);*/
 };
